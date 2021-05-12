bd46a3d5d486309152732e4f43cea489910fe034
ManoPandey <mano191091@gmail.com>
2021-05-13 01:36:33 +0800

    first assignment

---
+++ Python Programming Basic Assignment 1.py
@@ -0,0 +1,27 @@
+#Question 1
+print("Hello Python")
+
+#Question 2
+x = 5
+y = 7
+addition = x+y
+division = y/x
+print(addition)
+print(division)
+
+#Question 3
+b = 3
+h = 5
+area_triangle = (b*h)/2
+print(area_triangle)
+
+#Question 4
+a = 5
+b = 7
+a,b = b,a
+print (a,b)
+
+#Question 5
+from random import random
+random_num = random()
+print(random_num)
